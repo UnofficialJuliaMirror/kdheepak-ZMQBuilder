f7fd9d4f323c037ea16caf5aad2a9612731f3aad
Steven G. Johnson <stevenj@alum.mit.edu>
2018-04-06 00:05:38 -0400

    re-enable builds now that mac builds seem to be working

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -19,13 +19,13 @@ make && make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-#    Linux(:i686, :glibc),
-#    Linux(:x86_64, :glibc),
-#    Linux(:aarch64, :glibc),
-#    Linux(:armv7l, :glibc),
-#    Linux(:powerpc64le, :glibc),
-#    Windows(:x86_64),
-#    Windows(:i686),
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc),
+    Linux(:powerpc64le, :glibc),
+    Windows(:x86_64),
+    Windows(:i686),
     MacOS()
 ]
 
