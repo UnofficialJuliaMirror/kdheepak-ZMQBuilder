e86aea71d0088f271a7789d7e4e8cd1a82f39e50
Dheepak Krishnamurthy <me@kdheepak.com>
2019-10-07 21:12:45 -0600

    Patch ZMQ build on linux x86_64 musl gcc4 and expand gcc versions

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -8,7 +8,33 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
+
 cd $WORKSPACE/srcdir/libzmq
+
+if [[ ${target} == x86_64-linux-musl ]] && [[ $(gcc --version | head -1 | awk '{ print $3 }') == "4.8.5" ]]; then
+
+cat > mm_malloc.patch << 'END'
+--- mm_malloc.h
++++ mm_malloc.h
+@@ -31,7 +31,7 @@
+ #ifndef __cplusplus
+ extern int posix_memalign (void **, size_t, size_t);
+ #else
+-extern "C" int posix_memalign (void **, size_t, size_t) throw ();
++extern "C" int posix_memalign (void **, size_t, size_t);
+ #endif
+
+ static __inline void *
+END
+
+cd /opt/x86_64-linux-musl/lib/gcc/x86_64-linux-musl/4.8.5/include/
+
+patch < "${WORKSPACE}/srcdir/libzmq/mm_malloc.patch"
+
+cd $WORKSPACE/srcdir/libzmq
+
+fi
+
 sh autogen.sh
 ./configure --prefix=$prefix --host=${target} \
     --without-docs --disable-libunwind --disable-perf --disable-Werror \
@@ -21,6 +47,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms() # build on all supported platforms
+platforms = expand_gcc_versions(platforms)
 
 # The products that we will ensure are always built
 products(prefix) = [
